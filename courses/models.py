df1084eb3b60b2579fd460862899d338669604ca
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-07-13 17:24:30 +0700

    update

---
+++ courses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='')
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, null=False, unique=True)
+
+class Course(models.Model):
+    subject = models.CharField(max_length=100, null=False)
+    descreption = models.TextField(null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models . SET_NULL, null=True)
