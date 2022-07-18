d6e6e7ffdb9f8340707aa7312518b309312804ef
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2022-07-18 17:34:17 +0700

    update

--- courses/models.py
+++ courses/models.py
@@ -10,17 +10,24 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Course(models.Model):
+class ItemBase(models.Model):
+    class Meta:
+        abstract = True
     subject = models.CharField(max_length=100, null=False)
-    descreption = models.TextField(null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+class Course(ItemBase):
+    class Meta:
+        unique_together = ('subject', 'category')
+    descreption = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models . SET_NULL, null=True)
 
-class Lesson(models.Model):
-    subject = models.CharField(max_length=100, null=False)
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+class Lesson(ItemBase):
+    class Meta:
+        unique_together = ('subject','course')
+        db_table =
+    content = models.TextField()
+    image = models.ImageField(upload_to='courses/%Y/%m')
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
